fec114b49fba0b50b6352cc323b734a099b64ee9
Okan Bayram <okanbayram85@icloud.com>
2024-12-29 19:08:07 +0300

    Initial Commit

---
+++ SwiftTextAskMeAnything/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftTextAskMeAnything
+//
+//  Created by okan bayram on 29.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
