a95ceac45b06e161138e2c1b028556db4a3c1bfd
Okan Bayram <okanbayram85@icloud.com>
2024-12-29 20:17:55 +0300

    AskMeAnythin Project v1

--- SwiftTextAskMeAnything/ViewController.swift
+++ SwiftTextAskMeAnything/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var answerImage = ["ball1", "ball2", "ball3", "ball4"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func askButtonClicked(_ sender: Any) {
+        var number = Int.random(in: 0..<3)
+        imageView.image = UIImage(named: answerImage[number])
+    }
+    
 }
 
